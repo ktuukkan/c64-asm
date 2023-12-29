38197979b53d261479308e0dadc3ae393e01aa1c
Kimmo <kimmo.tuukkanen@gmail.com>
2023-12-29 14:22:27 +0200

    raster irq and cleanup

--- scroller/sprite-scroll.asm
+++ scroller/sprite-scroll.asm
@@ -1,68 +1,80 @@
 !cpu 6502
 !to "sprite-scroll.prg",cbm
 
-chr_loc     = $3800
-chr_mask    = chr_loc / $0800 * $02
-chr_ptr     = $fb
+spr_width   = 24
+spr_x       = 80
+spr_y       = 140
 
 spr_loc     = $2000
 spr_idx     = spr_loc / $40
 
-spr_width   = 24
-spr_x       = 80
-spr_y       = 140
-spr1        = spr_loc
-spr2        = spr1+64*1
-spr3        = spr1+64*2
-spr4        = spr1+64*3
-spr5        = spr1+64*4
-spr6        = spr1+64*5
-spr7        = spr1+64*6
-spr8        = spr1+64*7
+chr_loc     = $3800
+chr_mask    = chr_loc / $0800 * $02
 
-text_ptr    = $fd
+chr_ptr     = $fb       ; zp pointer for charset data
+text_ptr    = $fd       ; zp pointer for scroll text
 
 * = $0801
     !byte $0d,$08,$dc,$07,$9e,$20,$34
     !byte $39,$31,$35,$32,$00,$00,$00
 
 * = spr_loc
-    !fill 8*8*8, $00
+    !fill 8*8*8, $00    ; reserve/clear mem for 8 sprites
 
 * = chr_loc
     !bin "../bitmap-txt/charset.chr"
 
 * = $c000
-    lda #<text
-    sta text_ptr
-    lda #>text
-    sta text_ptr+1
-    lda #<chr_loc
-    sta chr_ptr
-    lda #>chr_loc
-    sta chr_ptr+1
+    jsr init_pointers
     jsr init_sprites
-    jsr insert_char
--   lda #200
-    cmp $d012
-    bne -
-    dec $d020
-    lda scroll_idx
-    cmp #8
-    bne ++
-+   jsr insert_char
-    lda #0
-    sta scroll_idx
-++  jsr scroll_text
-    inc scroll_idx
-    inc $d020
-    jmp -
+    jsr init_irq
+    jmp *
 
 scroll_idx
     !byte 0
 
+init_pointers
+    ; scroll text pointer
+    lda #<text
+    sta text_ptr
+    lda #>text
+    sta text_ptr+1
+    ; char data pointer
+    lda #<chr_loc
+    sta chr_ptr
+    lda #>chr_loc
+    sta chr_ptr+1
+    rts
+
+init_irq
+    sei
+    ldy #$7f
+    sty $dc0d
+    sty $dd0d
+    lda $dc0d
+    lda $dd0d
+    lda #$01
+    sta $d01a
+    lda $d011
+    and #$7f
+    sta $d011
+    lda #<irq
+    ldx #>irq
+    sta $314
+    stx $315
+    ldy #$1b
+    sty $d011
+    lda #$7f
+    sta $dc0d
+    lda #$01
+    sta $d01a
+    lda #200
+    sta $d012
+    cli
+    rts
+
 init_sprites
-    ;; sprite pointers, first mem location as sprite 7
+    ; sprite pointers, first mem location as sprite 7
     lda #spr_idx
     sta $07ff
     lda #spr_idx+1
@@ -79,13 +91,13 @@ init_sprites
     sta $07f9
     lda #spr_idx+7
     sta $07f8
-    ;; enable all
+    ; enable all
     lda #$ff
     sta $d015
-    ;; hires
+    ; hires
     lda #$00
     sta $d01c
-    ;; main color
+    ; main color
     lda #$01
     sta $d027
     sta $d028
@@ -95,10 +107,10 @@ init_sprites
     sta $d02c
     sta $d02d
     sta $d02e
-    ;; x high bits
+    ; x high bits
     lda #%00000000
     sta $d010
-    ;; x location
+    ; x location
     ldx #spr_x+spr_width*7
     stx $d00e
     ldx #spr_x+spr_width*6
@@ -115,7 +127,7 @@ init_sprites
     stx $d002
     ldx #spr_x
     stx $d000
-    ;; y location
+    ; y location
     ldy #spr_y
     sty $d001
     sty $d003
@@ -127,68 +139,69 @@ init_sprites
     sty $d00f
     rts
 
+irq
+    asl $d019
+    dec $d020
+    lda scroll_idx
+    cmp #8
+    bne +
+    jsr insert_char
+    lda #0
+    sta scroll_idx
++   jsr scroll_text
+    inc $d020
+    jmp $ea81
+
 insert_char
-    ;; reset charset pointer (allows using the first 64 chars)
-    lda #<chr_loc
-    sta chr_ptr
+    ; reset char pointer hi-byte (allows using the first 64 chars)
     lda #>chr_loc
     sta chr_ptr+1
-    ;; get character to insert
+
+    ; get character to insert
     ldy #0
     lda (text_ptr),y
-    ;; the end?
+    ; the end?
     cmp #0
     bne +
+    ; reset text pointer to loop over
     lda #<text
     sta text_ptr
     lda #>text
     sta text_ptr+1
     lda (text_ptr),y
 
-+   ;; char code * 8 = char shape offset from chr_loc
++   ; char code * 8 = char data offset relative to chr_loc
     clc
     rol
     rol
     rol
-    ;; set char shape pointer
+    ; set char data pointer
     sta chr_ptr
     bcc +
     inc chr_ptr+1
 
-+   ;; copy shape to rightmost sprite (top right corner)
-    ldy #0
-    lda (chr_ptr),y
-    sta spr_loc+2
++   ; copy char data to sprite on right (starting at top right / third byte)
+    ldy #0  ; char bytes counter
+    ldx #2  ; sprite bytes counter (sprite "line" consists of 3 bytes)
+-   lda (chr_ptr),y
+    sta spr_loc,x
+    inx
+    inx
+    inx
     iny
-    lda (chr_ptr),y
-    sta spr_loc+5
-    iny
-    lda (chr_ptr),y
-    sta spr_loc+8
-    iny
-    lda (chr_ptr),y
-    sta spr_loc+11
-    iny
-    lda (chr_ptr),y
-    sta spr_loc+14
-    iny
-    lda (chr_ptr),y
-    sta spr_loc+17
-    iny
-    lda (chr_ptr),y
-    sta spr_loc+20
-    iny
-    lda (chr_ptr),y
-    sta spr_loc+23
+    cpy #8
+    bne -
 
-    ;; prepare text pointer for next char
+    ; increment pointer for next char
     inc text_ptr
     bne +
     inc text_ptr+1
 +   rts
 
 scroll_text
-    !for row, 0, 8 {
+    ; shift sprite lines left on each sprite, carry will
+    ; pass overflown bits to next sprite
+    !for row, 0, 6 {
         clc
         !for sprite, 0, 7 {
             rol spr_loc + sprite * 64 + row * 3 + 2
@@ -196,6 +209,7 @@ scroll_text
             rol spr_loc + sprite * 64 + row * 3
         }
     }
+    inc scroll_idx
     rts
 
 text
